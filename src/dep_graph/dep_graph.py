6e5bfd4a8c50e13ec637dbc7b7ea6c032222eddf
Emmanuel Pauchard <emmanuel.pauchard@gmail.com>
2023-04-23 16:08:35 +0200

    Add dep_graph module to provide CLI

---
+++ src/dep_graph/dep_graph.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env ipython3
+
+import argparse
+
+from analyzer import Analyzer
+from parser import JsonParser
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Simple Dependency Graph analyzer.")
+    parser.add_argument(
+        "input",
+        nargs="?",
+        default="-",
+        type=argparse.FileType("r"),
+        help="Input file containing package structure in JSON description. Use '-' for stdin (default).",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    Analyzer(JsonParser(args.input.read()).parse()).run()
